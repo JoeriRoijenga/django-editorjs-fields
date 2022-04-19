e3bb3f4d4aa97cb6fa28b35c571acc9b3a3a91b2
Joeri Roijenga <joeri.12@live.nl>
2022-04-19 11:06:10 +0200

    removed example file and name change in .\toml

--- example/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import PostUpdate, PostView
-
-urlpatterns = [
-    path('posts/<int:pk>/edit', PostUpdate.as_view(), name='post_edit'),
-    path('posts/<int:pk>', PostView.as_view(), name='post_detail'),
-]
